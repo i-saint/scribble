d9cf78f98fccf9859e1c90ed68a5bde3c744af14
i-saint <saint.skr@gmail.com>
2012-10-16 00:50:57 +0900

    RPC

--- RPC/RPC.cpp
+++ RPC/RPC.cpp
@@ -1,24 +1,29 @@
 #include <cstdio>
+#include <cstdlib>
 #include <cstring>
-#include <algorithm>
+#include <cassert>
+#include <stdint.h>
+#include <intrin.h>
 
 template<size_t Size>
-struct ArgmentData
+struct ArgData
 {
-    ArgmentData() {}
+    ArgData() {}
 
     template<typename T>
-    ArgmentData(const T &arg)
+    ArgData(const T &arg)
     {
         assert(sizeof(T) <= Size);
         *((T*)data) = arg;
     }
 
     template<typename T, size_t S>
-    ArgmentData(const T (&arg)[S])
+    ArgData(const T (&arg)[S])
     {
-        assert(sizeof(T)*s <= Size);
-        std::copy(arg, arg+S, (T*)data);
+        assert(sizeof(T)*S <= Size);
+        for(size_t i=0; i<S; ++i) {
+            ((T*)data)[i] = arg[i];
+        }
     }
 
     char data[Size];
@@ -30,81 +35,174 @@ enum ArgTypes {
     ArgType_Other,
 };
 
-struct ArgmentTypeInfo
+struct ArgTypeInfo
 {
-    int type; // ArgTypes
-    int size;
+    size_t type; // ArgTypes
+    size_t size;
+
+    template<typename T> static void Create(ArgTypeInfo &t) { t.type=ArgType_Other; t.size=(sizeof(T)+15) & ~15; }
+    template<> static void Create<  int8_t>(ArgTypeInfo &t) { t.type=  ArgType_Int; t.size=16; }
+    template<> static void Create< uint8_t>(ArgTypeInfo &t) { t.type=  ArgType_Int; t.size=16; }
+    template<> static void Create< int16_t>(ArgTypeInfo &t) { t.type=  ArgType_Int; t.size=16; }
+    template<> static void Create<uint16_t>(ArgTypeInfo &t) { t.type=  ArgType_Int; t.size=16; }
+    template<> static void Create< int32_t>(ArgTypeInfo &t) { t.type=  ArgType_Int; t.size=16; }
+    template<> static void Create<uint32_t>(ArgTypeInfo &t) { t.type=  ArgType_Int; t.size=16; }
+    template<> static void Create< int64_t>(ArgTypeInfo &t) { t.type=  ArgType_Int; t.size=16; }
+    template<> static void Create<uint64_t>(ArgTypeInfo &t) { t.type=  ArgType_Int; t.size=16; }
+    template<> static void Create<   float>(ArgTypeInfo &t) { t.type=ArgType_Float; t.size=16; }
+    template<> static void Create<  double>(ArgTypeInfo &t) { t.type=ArgType_Float; t.size=16; }
+    template<> static void Create<  __m128>(ArgTypeInfo &t) { t.type=ArgType_Float; t.size=16; }
+
+    template<typename T> static void CopyData(void *d, const T v)          { memcpy(d, &v, sizeof(T)); }
+    template<> static void CopyData<  int8_t>(void *d, const int8_t v)     { *(uint32_t*)d = v; }
+    template<> static void CopyData< uint8_t>(void *d, const uint8_t v)    { *(uint32_t*)d = v; }
+    template<> static void CopyData< int16_t>(void *d, const int16_t v)    { *(uint32_t*)d = v; }
+    template<> static void CopyData<uint16_t>(void *d, const uint16_t v)   { *(uint32_t*)d = v; }
+    template<> static void CopyData< int32_t>(void *d, const int32_t v)    { *(uint32_t*)d = v; }
+    template<> static void CopyData<uint32_t>(void *d, const uint32_t v)   { *(uint32_t*)d = v; }
+    template<> static void CopyData< int64_t>(void *d, const int64_t v)    { *(uint32_t*)d = v; }
+    template<> static void CopyData<uint64_t>(void *d, const uint64_t v)   { *(uint32_t*)d = v; }
+    template<> static void CopyData<   float>(void *d, const float v)      { *(__m128d*)d = _mm_set_sd(v); }
+    template<> static void CopyData<  double>(void *d, const double v)     { *(__m128d*)d = _mm_set_sd(v); }
+    template<> static void CopyData<  __m128>(void *d, const __m128 v)     { *(__m128*)d = v; }
 };
 
-struct APCInfo
+struct RPCInfo
 {
     char module_name[64];
     void *function;
-    int num_args;
-    // ArgmentTypeInfo types[num_args];
-    // ArgmentData<types[0].size> data[0]
-    // ArgmentData<types[1].size> data[1]
+    size_t num_args;
+    // ArgTypeInfo types[num_args];
+    // ArgData<types[0].size> data[0]
+    // ArgData<types[1].size> data[1]
     // ...
 };
 
 
 template<typename Ret>
-APCInfo* CreateRPCInfo(Ret (*f)())
+RPCInfo* CreateRPCInfo(Ret (*f)())
 {
-    size_t num_args = 0;
-    size_t alloc_size = sizeof(ArgmentTypeInfo) + sizeof(APCInfo)*num_args;
+    size_t alloc_size = sizeof(ArgTypeInfo);
+    RPCInfo *ret = (RPCInfo*)_aligned_malloc(alloc_size, 16);
+    ret->function = f;
+    ret->num_args = 0;
+
+    return ret;
 }
 
 template<typename Ret, typename Arg1>
-APCInfo* CreateRPCInfo(Ret (*f)(Arg1), Arg1 arg1)
+RPCInfo* CreateRPCInfo(Ret (*f)(Arg1), Arg1 arg1)
 {
-    size_t num_args = 1;
-    size_t alloc_size = sizeof(ArgmentTypeInfo) + sizeof(APCInfo)*num_args
-        + sizeof(Arg1);
+    ArgTypeInfo types[1];
+    ArgTypeInfo::Create<Arg1>(types[0]);
+
+    size_t alloc_size = sizeof(ArgTypeInfo) + sizeof(RPCInfo)*_countof(types) +
+                        types[0].size;
+    RPCInfo *ret = (RPCInfo*)_aligned_malloc(alloc_size, 16);
+    ret->function = f;
+    ret->num_args = _countof(types);
+
+    ArgTypeInfo *t = (ArgTypeInfo*)(ret+1);
+    char *d = (char*)(t+_countof(types));
+    for(size_t i=0; i<_countof(types); ++i) { t[i]=types[i]; }
+    ArgTypeInfo::CopyData(d, arg1); d+=types[0].size;
+
+    return ret;
 }
 
 template<typename Ret, typename Arg1, typename Arg2>
-APCInfo* CreateRPCInfo(Ret (*f)(Arg1, Arg2), Arg1 arg1, Arg2 arg2)
+RPCInfo* CreateRPCInfo(Ret (*f)(Arg1, Arg2), Arg1 arg1, Arg2 arg2)
 {
-    size_t num_args = 2;
-    size_t alloc_size = sizeof(ArgmentTypeInfo) + sizeof(APCInfo)*num_args
-        + sizeof(Arg1) + sizeof(Arg2);
+    ArgTypeInfo types[2];
+    ArgTypeInfo::Create<Arg1>(types[0]);
+    ArgTypeInfo::Create<Arg2>(types[1]);
+
+    size_t alloc_size = sizeof(ArgTypeInfo) + sizeof(RPCInfo)*_countof(types) +
+                        types[0].size + types[1].size;
+    RPCInfo *ret = (RPCInfo*)_aligned_malloc(alloc_size, 16);
+    ret->function = f;
+    ret->num_args = _countof(types);
+
+    ArgTypeInfo *t = (ArgTypeInfo*)(ret+1);
+    char *d = (char*)(t+_countof(types));
+    for(size_t i=0; i<_countof(types); ++i) { t[i]=types[i]; }
+    ArgTypeInfo::CopyData(d, arg1); d+=types[0].size;
+    ArgTypeInfo::CopyData(d, arg2); d+=types[1].size;
+
+    return ret;
 }
 
 template<typename Ret, typename Arg1, typename Arg2, typename Arg3>
-APCInfo* CreateRPCInfo(Ret (*f)(Arg1, Arg2, Arg3), Arg1 arg1, Arg2 arg2, Arg3 arg3)
+RPCInfo* CreateRPCInfo(Ret (*f)(Arg1, Arg2, Arg3), Arg1 arg1, Arg2 arg2, Arg3 arg3)
 {
-    size_t num_args = 3;
-    size_t alloc_size = sizeof(ArgmentTypeInfo) + sizeof(APCInfo)*num_args
-        + sizeof(Arg1) + sizeof(Arg2) + sizeof(Arg3);
+    ArgTypeInfo types[3];
+    ArgTypeInfo::Create<Arg1>(types[0]);
+    ArgTypeInfo::Create<Arg2>(types[1]);
+    ArgTypeInfo::Create<Arg3>(types[2]);
+
+    size_t alloc_size = sizeof(ArgTypeInfo) + sizeof(RPCInfo)*_countof(types) +
+                        types[0].size + types[1].size + types[2].size;
+    RPCInfo *ret = (RPCInfo*)_aligned_malloc(alloc_size, 16);
+    ret->function = f;
+    ret->num_args = _countof(types);
+
+    ArgTypeInfo *t = (ArgTypeInfo*)(ret+1);
+    char *d = (char*)(t+_countof(types));
+    for(size_t i=0; i<_countof(types); ++i) { t[i]=types[i]; }
+    ArgTypeInfo::CopyData(d, arg1); d+=types[0].size;
+    ArgTypeInfo::CopyData(d, arg2); d+=types[1].size;
+    ArgTypeInfo::CopyData(d, arg3); d+=types[2].size;
+
+    return ret;
 }
 
 template<typename Ret, typename Arg1, typename Arg2, typename Arg3, typename Arg4>
-APCInfo* CreateRPCInfo(Ret (*f)(Arg1, Arg2, Arg3, Arg4), Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4)
+RPCInfo* CreateRPCInfo(Ret (*f)(Arg1, Arg2, Arg3, Arg4), Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4)
 {
-    size_t num_args = 4;
-    size_t alloc_size = sizeof(ArgmentTypeInfo) + sizeof(APCInfo)*num_args
-        + sizeof(Arg1) + sizeof(Arg2) + sizeof(Arg3) + sizeof(Arg4);
+    ArgTypeInfo types[4];
+    ArgTypeInfo::Create<Arg1>(types[0]);
+    ArgTypeInfo::Create<Arg2>(types[1]);
+    ArgTypeInfo::Create<Arg3>(types[2]);
+    ArgTypeInfo::Create<Arg3>(types[3]);
+
+    size_t alloc_size = sizeof(ArgTypeInfo) + sizeof(RPCInfo)*_countof(types) +
+                        types[0].size + types[1].size + types[2].size + types[3].size;
+    RPCInfo *ret = (RPCInfo*)_aligned_malloc(alloc_size, 16);
+    ret->function = f;
+    ret->num_args = _countof(types);
+
+    ArgTypeInfo *t = (ArgTypeInfo*)(ret+1);
+    char *d = (char*)(t+_countof(types));
+    for(size_t i=0; i<_countof(types); ++i) { t[i]=types[i]; }
+    ArgTypeInfo::CopyData(d, arg1); d+=types[0].size;
+    ArgTypeInfo::CopyData(d, arg2); d+=types[1].size;
+    ArgTypeInfo::CopyData(d, arg3); d+=types[2].size;
+    ArgTypeInfo::CopyData(d, arg4); d+=types[3].size;
+
+    return ret;
 }
 
 extern "C" void RPCCall(const void *args);
 
 
-
-void TestFunc(void* arg1, short arg2, ArgmentData<8> arg3, float arg4)
+void Test1(int i)
 {
-    printf("hoge\n");
+    printf("Test1(%d)\n", i);
 }
 
 
 int main()
 {
-    struct Test {
-        void *fp;
-        const char *format;
-        size_t idata;
-    } testdata = {&printf, "data: %d\n", 100};
-    RPCCall(&testdata);
-
-    TestFunc(NULL, 10, ArgmentData<8>(), 10.0f);
+    {
+        RPCInfo *ri = CreateRPCInfo(&Test1, 100);
+        RPCCall(ri);
+        _aligned_free(ri);
+    }
+    {
+        typedef int (*FP)(ArgData<64>, int, float);
+        char format[64] = "test %d %.2f\n";
+        RPCInfo *ri = CreateRPCInfo((FP)&printf, ArgData<64>(format), 10, 100.0f);
+        RPCCall(ri);
+        _aligned_free(ri);
+    }
 }
--- RPC/RPCCall_x64.asm
+++ RPC/RPCCall_x64.asm
@@ -1,10 +1,107 @@
 .code
 RPCCall PROC
-    mov rax, rcx
-    mov rcx, qword ptr [rax +  08h]
-    mov rdx, qword ptr [rax + 010h]
-    mov r8,  qword ptr [rax + 018h]
-    mov r9,  qword ptr [rax + 018h]
-    jmp qword ptr [rax + 00h]
+    mov rax, rcx                    ; rax: RPCInfo
+
+    mov r10, qword ptr [rax+48h] ; r10: num_args
+
+    mov r11, rax    ; r11: pointer to ArgTypeInfo[]
+    add r11, 50h
+
+    mov r12, 10h
+    imul r12, r10
+    add r12, rax
+    add r12, 50h ; r12: pointer to data
+    xor r13, r13
+
+    cmp r10, 0
+    je Jump@RPCCall
+
+    mov r13, qword ptr [r11+0] ; r13 arg type
+    cmp r13, 1
+    je Arg1PassFloat@RPCCall
+    cmp r13, 2
+    je Arg1PassObj@RPCCall
+Arg1PassInt@RPCCall:
+    mov rcx, qword ptr [r12]
+    jmp Arg1End@RPCCall
+Arg1PassFloat@RPCCall:
+    movsd xmm0, mmword ptr [r12]
+    jmp Arg1End@RPCCall
+Arg1PassObj@RPCCall:
+    mov rcx, r12
+Arg1End@RPCCall:
+    mov r13, qword ptr [r11+8] ; r13 arg size
+    add r12, r13
+    add r11, 10h
+
+    cmp r10, 1
+    je Jump@RPCCall
+
+    mov r13, qword ptr [r11+0] ; r13 arg type
+    cmp r13, 1
+    je Arg2PassFloat@RPCCall
+    cmp r13, 2
+    je Arg2PassObj@RPCCall
+Arg2PassInt@RPCCall:
+    mov rdx, qword ptr [r12]
+    jmp Arg2End@RPCCall
+Arg2PassFloat@RPCCall:
+    movsd xmm1, mmword ptr [r12]
+    jmp Arg2End@RPCCall
+Arg2PassObj@RPCCall:
+    mov rdx, r12
+Arg2End@RPCCall:
+    mov r13, qword ptr [r11+8] ; r13 arg size
+    add r12, r13
+    add r11, 10h
+
+
+    cmp r10, 2
+    je Jump@RPCCall
+
+    mov r13, qword ptr [r11+0] ; r13 arg type
+    cmp r13, 1
+    je Arg3PassFloat@RPCCall
+    cmp r13, 2
+    je Arg3PassObj@RPCCall
+Arg3PassInt@RPCCall:
+    mov r8, qword ptr [r12]
+    jmp Arg3End@RPCCall
+Arg3PassFloat@RPCCall:
+    movsd xmm2, mmword ptr [r12]
+    movd r8, xmm2
+    jmp Arg3End@RPCCall
+Arg3PassObj@RPCCall:
+    mov r8, r12
+Arg3End@RPCCall:
+    mov r13, qword ptr [r11+8] ; r13 arg size
+    add r12, r13
+    add r11, 10h
+
+    cmp r10, 3
+    je Jump@RPCCall
+
+
+    mov r13, qword ptr [r11+0] ; r13 arg type
+    cmp r13, 1
+    je Arg4PassFloat@RPCCall
+    cmp r13, 2
+    je Arg4PassObj@RPCCall
+Arg4PassInt@RPCCall:
+    mov r9, qword ptr [r12]
+    jmp Arg4End@RPCCall
+Arg4PassFloat@RPCCall:
+    movsd xmm3, mmword ptr [r12]
+    jmp Arg4End@RPCCall
+Arg4PassObj@RPCCall:
+    mov r9, r12
+Arg4End@RPCCall:
+    mov r13, qword ptr [r11+8] ; r13 arg size
+    add r12, r13
+    add r11, 10h
+
+
+Jump@RPCCall:
+    jmp qword ptr [rax+40h]
 RPCCall ENDP
 END
