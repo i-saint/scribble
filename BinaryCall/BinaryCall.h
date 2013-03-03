811042b230775d9af2ef3f52d35331a65d066576
i-saint <saint.skr@gmail.com>
2013-03-03 23:00:43 +0900

    binary call

--- BinaryCall/BinaryCall.h
+++ BinaryCall/BinaryCall.h
@@ -2,14 +2,6 @@
 #define BinaryCall_h
 #include <type_traits>
 
-typedef char                int8;
-typedef short               int16;
-typedef int                 int32;
-typedef long long           int64;
-typedef unsigned char       uint8;
-typedef unsigned short      uint16;
-typedef unsigned int        uint32;
-typedef unsigned long long  uint64;
 
 #define RemoveCR(T) typename std::remove_const<typename std::remove_reference<T>::type>::type
 
