e2bad04de51c23aa525011843c4b41b0d4febd8c
i-saint <saint.skr@gmail.com>
2013-07-24 20:00:18 +0900

    is_complete.cpp

---
+++ is_complete.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+
+template<class T>
+struct is_complete
+{
+// VisualC++ only
+// other approach: http://stackoverflow.com/questions/1625105/how-to-write-is-complete-template/1625360
+static const bool value = __alignof(T)!=0;
+};
+
+class Hoge;
+class Hage {};
+
+
+int main()
+{
+    printf("is_complete<Hoge>: %d\n", is_complete<Hoge>::value);
+    printf("is_complete<Hage>: %d\n", is_complete<Hage>::value);
+}
+
+// $ cl incomplete_type.cpp && ./incomplete_type
+// is_complete<Hoge>: 0
+// is_complete<Hage>: 1
