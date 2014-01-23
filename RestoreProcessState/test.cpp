f16ad41f1a72ecabb1a99fb15c1cb3ff9a58d379
i-saint <saint.skr@gmail.com>
2014-01-23 23:00:11 +0900

    rps: コメントとか追記

--- RestoreProcessState/test.cpp
+++ RestoreProcessState/test.cpp
@@ -9,7 +9,7 @@ void func1(int stack_variable)
     std::string local_variable = "abc";
     int *heap_variable = new int(123456);
 
-    rpsSaveState("test.stat"); // 1. save state at this time
+    rpsSaveState("test.stat"); // 1. この時点の状態を保存
 
     global_variable += " func1()";
 
@@ -18,6 +18,9 @@ void func1(int stack_variable)
             "  heap_variable: %d\n"
             "  stack_variable: %d\n"
             , global_variable.c_str(), *heap_variable, stack_variable );
+
+    local_variable.clear();
+    *heap_variable = 0;
 }
 
 void func2()
@@ -29,7 +32,7 @@ void func2()
             , global_variable.c_str());
 
     ::Sleep(2000);
-    rpsLoadState("test.stat"); // 2. restore state
+    rpsLoadState("test.stat"); // 2. 1. の状態を復元 (メモリの状態やプログラムカウンタが 1. の時点に戻る)
 }
 
 
@@ -65,5 +68,5 @@ func1():
 func2():
   g_global_variable: g func1() func2()
 
-  ...
+  ... (無限ループ)
 */
