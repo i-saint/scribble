daf1e4e320aff3153120903717140d7308dcde05
i-saint <saint.skr@gmail.com>
2015-01-14 18:17:13 +0900

    ispc

--- ispc/build.sh
+++ ispc/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./ispc --arch=x86-64 --addressing=64 --target=avx -h test.h --emit-obj -o out.obj --opt=fast-masked-vload --opt=fast-math --opt=force-aligned-memory test.ispc
+./ispc --arch=x86-64 --addressing=64 --target=avx -h test.h --emit-obj -o out.obj --opt=fast-masked-vload --opt=fast-math --opt=force-aligned-memory test.ispc && \
 dumpbin /disasm out.obj
