03441bbad981b26d1f3dfa990d543379be905003
i-saint <saint.skr@gmail.com>
2022-07-30 13:50:51 +0900

    ExecuteWithDLLInjection.cpp

--- DLLInjection/ExecuteWithDLLInjection.cpp
+++ DLLInjection/ExecuteWithDLLInjection.cpp
@@ -1,16 +1,19 @@
 ﻿#pragma comment(lib, "comdlg32.lib")
 #include "windows.h"
+#include <filesystem>
 
 bool InjectDLL(HANDLE hProcess, const char* dllname)
 {
     SIZE_T bytesRet = 0;
     DWORD oldProtect = 0;
-    LPVOID remote_addr = NULL;
-    HANDLE hThread = NULL;
+    LPVOID remote_addr = nullptr;
+    HANDLE hThread = nullptr;
     size_t len = strlen(dllname) + 1;
 
     remote_addr = ::VirtualAllocEx(hProcess, 0, 1024, MEM_COMMIT|MEM_RESERVE, PAGE_EXECUTE_READWRITE);
-    if(remote_addr==NULL) { return false; }
+    if(!remote_addr)
+        return false;
+
     ::VirtualProtectEx(hProcess, remote_addr, len, PAGE_EXECUTE_READWRITE, &oldProtect);
     ::WriteProcessMemory(hProcess, remote_addr, dllname, len, &bytesRet);
     ::VirtualProtectEx(hProcess, remote_addr, len, oldProtect, &oldProtect);
@@ -21,51 +24,64 @@ bool InjectDLL(HANDLE hProcess, const char* dllname)
     return true;
 }
 
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE prev, LPWSTR cmd, int show)
+int main(int argc, char *argv[])
 {
-    char exePath[MAX_PATH] = {0};
-    char dllPath[MAX_PATH] = {0};
+    char exePath[MAX_PATH]{};
+    char dllPath[MAX_PATH]{};
 
-    {
-        OPENFILENAMEA fn;
-        ::ZeroMemory(&fn, sizeof(fn));
-        fn.lStructSize = sizeof(fn);
-        fn.lpstrTitle = "select .dll to inject";
-        fn.lpstrFilter = "DLL\0*.dll\0";
-        fn.nFilterIndex = 1;
-        fn.lpstrFile = dllPath;
-        fn.nMaxFile = MAX_PATH;
-        fn.Flags = OFN_PATHMUSTEXIST|OFN_FILEMUSTEXIST;
-        if(!GetOpenFileNameA(&fn)) {
-            return 0;
+    // コマンドライン引数その 1 は実行する exe のパス、その 2 は inject される dll のパスと。
+    // コマンドライン引数がない場合はファイルダイアログを出して指定させる。
+
+    if (argc >= 3) {
+        strcpy(exePath, argv[1]);
+        strcpy(dllPath, argv[2]);
+    }
+    else {
+        {
+            OPENFILENAMEA fn{};
+            fn.lStructSize = sizeof(fn);
+            fn.lpstrTitle = "select .exe to execute";
+            fn.lpstrFilter = "Executables\0*.exe\0";
+            fn.nFilterIndex = 1;
+            fn.lpstrFile = exePath;
+            fn.nMaxFile = MAX_PATH;
+            fn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+            if (!GetOpenFileNameA(&fn)) {
+                return -1;
+            }
+        }
+        {
+            OPENFILENAMEA fn{};
+            fn.lStructSize = sizeof(fn);
+            fn.lpstrTitle = "select .dll to inject";
+            fn.lpstrFilter = "DLL\0*.dll\0";
+            fn.nFilterIndex = 1;
+            fn.lpstrFile = dllPath;
+            fn.nMaxFile = MAX_PATH;
+            fn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+            if (!GetOpenFileNameA(&fn)) {
+                return -1;
+            }
         }
     }
-    {
-        OPENFILENAMEA fn;
-        ::ZeroMemory(&fn, sizeof(fn));
-        fn.lStructSize = sizeof(fn);
-        fn.lpstrTitle = "select .exe to execute";
-        fn.lpstrFilter = "Executables\0*.exe\0";
-        fn.nFilterIndex = 1;
-        fn.lpstrFile = exePath;
-        fn.nMaxFile = MAX_PATH;
-        fn.Flags = OFN_PATHMUSTEXIST|OFN_FILEMUSTEXIST;
-        if(!GetOpenFileNameA(&fn)) {
-            return 0;
-        }
+
+    if (!std::filesystem::exists(exePath) || !std::filesystem::exists(dllPath)) {
+        printf("usage: %s [exe path to execute] [dll path to be injected]", argv[0]);
+        return -1;
     }
 
-    STARTUPINFOA si;
-    PROCESS_INFORMATION pi;
-    ::ZeroMemory(&si, sizeof(si));
-    ::ZeroMemory(&pi, sizeof(pi));
+    STARTUPINFOA si{};
+    PROCESS_INFORMATION pi{};
     si.cb = sizeof(si);
     BOOL ret = ::CreateProcessA(exePath, NULL, NULL, NULL, FALSE,
-        NORMAL_PRIORITY_CLASS|CREATE_SUSPENDED, NULL, NULL, &si, &pi);
-    if(ret) {
-        InjectDLL(pi.hProcess, dllPath);
-        ::ResumeThread(pi.hThread);
+        NORMAL_PRIORITY_CLASS | CREATE_SUSPENDED, NULL, NULL, &si, &pi);
+    if (!ret) {
+        return -1;
     }
 
+    InjectDLL(pi.hProcess, dllPath);
+    ::ResumeThread(pi.hThread);
     return 0;
 }
+
+// cl ExecuteWithDLLInjection.cpp /std:c++17 /EHsc
